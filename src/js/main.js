7d3d784c34d2164782dfbaec31a41a19ac5f35c5
sasssssha <alex.dyomyshev@gmail.com>
2017-10-30 00:06:34 -0400

    refactoring

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,36 @@
-import './listeners'
+import _ from './pageItems'
+import fullscreen from 'fullscreen'
 
 
+_.fullScrBtn.addEventListener('click', () => {
+	fullscreen(_.html).request();
+})
+
+_.homeButton.addEventListener('click', () => {
+	_.main.style.display = "block";
+	_.about.style.display = "none";
+})
+
+_.aboutButton.addEventListener('click', () => {
+	_.main.style.display = "none";
+	_.about.style.display = "block";
+})
+
+_.toTileButton.addEventListener('click', () => {
+	toTileButton.classList.add("active");
+	toRowButton.classList.remove("active");
+	_.items.forEach((el) => {
+		el.className = 'tile'
+	})
+})
+
+_.toRowButton.addEventListener('click', () => {
+	toTileButton.classList.remove("active");
+	toRowButton.classList.add("active");
+	_.items.forEach((el) => {
+		el.className = 'item'
+	})
+})
 
 
 
---
+++ src/js/pageItems.js
@@ -0,0 +1,19 @@
+
+let getEl = (id) => (document.getElementById(id));
+
+let items = {};
+
+items.html = document.documentElement;
+items.homeButton = getEl('homeButton');
+items.aboutButton = getEl('aboutButton');
+items.fullScrBtn = getEl('fullscreen');
+items.toTileButton = getEl('toTileButton');
+items.toRowButton = getEl('toRowButton');
+items.polkaRapper = getEl('main-wrapper');
+items.items = [].slice.call(items.polkaRapper.childNodes).filter(el => el.tagName === 'DIV');
+items.main = getEl('main');
+items.about = getEl('about-container');
+
+
+
+export default items;
